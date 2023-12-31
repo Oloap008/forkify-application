12f3f2c72b639b56b678bb3437569d0865890447
Oloap008 <paolohenry008@gmail.com>
2023-12-31 15:50:20 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `ca788ec4-35fc-4b07-9a6b-3eab07810aa9`;
+export const MODAL_CLOSE_SEC = 2.5;
